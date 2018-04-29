2218a4c1915eb0c9a06010f78837c04fe58fa7d7
Elvira Miro <karashistka@gmail.com>
2018-04-29 15:46:56 +0300

    jws working example

--- test-jest/peatio/helpers/jws/sign.js
+++ test-jest/peatio/helpers/jws/sign.js
@@ -3,7 +3,13 @@ const base64 = require('base-64')
 const config = require('../../../config')
 
 module.exports = (payload, key_id) => {
-  let pem = base64.decode(config.MANAGEMENT_API_SIGNS[key_id].private_key)
+  payload = {
+    data: payload,
+    iat: Math.round(new Date().getTime() / 1000) - 1,
+    exp: Math.round(new Date().getTime() / 1000) + 4 * 3600,
+    jti: 'F454AF7134FC22506E06CF93'
+  }
+  let pem = config.MANAGEMENT_API_SIGNS[key_id].private_key
   let signData = jsonwebtoken.sign(payload, pem, {algorithm: config.MANAGEMENT_API_SIGNS[key_id].algorithm}).split(".")
   return {
     payload: base64.encode(JSON.stringify(payload)),
