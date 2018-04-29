360816fd5805214e8a27c920293fb9c2309fcc74
Elvira Miro <karashistka@gmail.com>
2018-04-29 18:05:41 +0300

    deposit & withdraw example

--- test-jest/peatio/jws.test.js
+++ test-jest/peatio/jws.test.js
@@ -9,6 +9,11 @@ describe('deposit tests', () => {
             added: [],
             result: []
         }
+        this.withdraws = {
+            start: [],
+            added: [],
+            result: []
+        }
     })
 
     test('Read config', done => {
@@ -19,20 +24,13 @@ describe('deposit tests', () => {
     })
     
     test('Try to get fiat deposits', done => {
+        // sign
         const signedDoc = jwsSign({
-            exp: Math.round(new Date().getTime() / 1000) + 4 * 3600,
-            sub: 'multisign',
-            aud: [
-            'peatio',
-            'barong'
-            ],
             uid: config.JWT_TEST_USER.uid
-            
         }, 'firstSign');
         
         management_api.post('/deposits',signedDoc).then(response => {
             this.deposits.start = response.data
-            console.log("START deposits", response.data)
             expect(response.status).toEqual(200)
             done()
         }).catch(error=> {
@@ -42,35 +40,56 @@ describe('deposit tests', () => {
 
     test('Fiat deposit', done => {
         let signedDoc = jwsSign({
-            exp: Math.round(new Date().getTime() / 1000) + 4 * 3600,
-            sub: 'multisign',
-            aud: [
-            'peatio',
-            'barong'
-            ],
             uid: config.JWT_TEST_USER.uid,
             currency: 'usd',
             amount: 100,
-            status: "accepted"
+            state: "accepted"
         }, 'firstSign')
         management_api.post('/deposits/new',signedDoc).then(response => {
             this.deposits.added.push(response.data)
-            console.log("ADD deposits", response.data)
             expect(response.status).toEqual(201)
+            expect(response.data.state).toEqual('accepted')
             let requestData = jwsSign({
-                exp: Math.round(new Date().getTime() / 1000) + 4 * 3600,
-                sub: 'multisign',
-                aud: [
-                'peatio',
-                'barong'
-                ],
                 uid: config.JWT_TEST_USER.uid
             }, 'firstSign')
             management_api.post('/deposits',requestData).then(response => {
                 this.deposits.result = response.data
-                console.log("Result deposits", response.data)
-                expect(response.status).toEqual(200)
+                expect(this.deposits.result.length - this.deposits.start.length).toEqual(this.deposits.added.length)
                 done()
+            }).catch(error=> {
+                console.log("ERROR", error)
+            })
+        }).catch(error=> {
+            console.log("ERROR", error)
+        })
+    })
+
+    test("Withdraws test", done => {
+        let requestData = jwsSign({
+            uid: config.JWT_TEST_USER.uid
+        }, 'firstSign')
+        management_api.post('/withdraws',requestData).then(response => {
+            this.withdraws.start = response.data
+            expect(response.status).toEqual(200)
+            let withdrawSign = jwsSign({
+                uid: config.JWT_TEST_USER.uid,
+                rid: config.JWT_TEST_USER.uid,
+                currency: 'usd',
+                amount: 1,
+                state: "submitted"
+            }, 'firstSign')
+            management_api.post('/withdraws/new',withdrawSign).then(response => {
+                console.log("RESULT", response.data)
+                this.withdraws.added.push(response.data)
+                expect(response.status).toEqual(201)
+                expect(response.data.state).toEqual('submitted')
+                management_api.post('/withdraws',requestData).then(response => {
+                    this.withdraws.result = response.data
+                    expect(this.withdraws.result.length - this.withdraws.start.length).toEqual(this.withdraws.added.length)
+                    done()
+                }).catch(error=> {
+                    console.log("ERROR", error)
+                })
             }).catch(error=> {
                 console.log("ERROR", error)
             })
